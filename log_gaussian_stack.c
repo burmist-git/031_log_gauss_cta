982a204fef9b8cbbccd56aae1648d6c8f4c84dd2
burmist <burm.leonid@gmail.com>
2021-07-30 16:15:16 +0200

    update

--- log_gaussian_stack.c
+++ log_gaussian_stack.c
@@ -7,6 +7,7 @@ const unsigned int yDim = 609;
 const unsigned int zDim = 38;
 
 void log_gaussian(const double (&x)[xDim][yDim][1], const double (&mean)[xDim][yDim][zDim], const double (&sigma)[xDim][yDim][zDim], double (&log_pdf)[xDim][yDim][zDim]);
+double makesum(const double (&log_pdf)[xDim][yDim][zDim]);
 
 int main(){
   TRandom3 *rnd = new TRandom3(1234567890);
@@ -42,6 +43,7 @@ int main(){
     log_gaussian(x, mean, sigma, log_pdf);
     clock_t toc = clock();
     tottime = tottime + (double)(toc - tic);
+    printf("sum %f \n", makesum(log_pdf));
   }
   printf("Elapsed: %f seconds\n", tottime / CLOCKS_PER_SEC / n);
   return 0;
@@ -55,4 +57,16 @@ void log_gaussian(const double (&x)[xDim][yDim][1], const double (&mean)[xDim][y
     for(j = 0;j<yDim;j++)
       for(k = 0;k<zDim;k++)
 	log_pdf[i][j][k] = -(x[i][j][0] - mean[i][j][k])*(x[i][j][0] - mean[i][j][k])/2.0/sigma[i][j][k]/sigma[i][j][k] - log(sqrt(2*3.141592653589793)*sigma[i][j][k]);
+}
+
+double makesum(const double (&log_pdf)[xDim][yDim][zDim]){
+  unsigned int i = 0;
+  unsigned int j = 0;
+  unsigned int k = 0;
+  double sum = 0;
+  for(i = 0;i<xDim;i++)
+    for(j = 0;j<yDim;j++)
+      for(k = 0;k<zDim;k++)
+	sum += log_pdf[i][j][k];
+  return sum;
 }
